2382e47995b112172541b2092821d975dbeae3da
Zenthae <zenthae@gmail.com>
2020-12-07 18:44:29 +0100

    add first root

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,8 @@
     Home Page, go to
     <nuxt-link to="components"><code>'/components'</code></nuxt-link> to see
     test the different components.
+
+    <nuxt-link to="first">APP</nuxt-link>
   </div>
 </template>
 
